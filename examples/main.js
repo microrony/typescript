fe12dae789cd891bfbd28675866781b06a3b53e4
Mahabubur Rahman <microronyy@gmail.com>
2020-12-28 20:16:43 +0600

    problem with banila javascript

---
+++ examples/main.js
@@ -0,0 +1,13 @@
+const button = document.querySelector('button')
+let input1 = document.getElementById('num1')
+let input2 = document.getElementById('num2')
+
+const add = (num1, num2) => {
+  return num1 + num2
+}
+
+const sum = () => {
+  console.log(add(input1.value, input2.value))
+}
+
+button.addEventListener('click', sum)
